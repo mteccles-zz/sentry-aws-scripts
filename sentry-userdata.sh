1e093004846c2a03e9f5f8d91bf448341fa50b8b
mteccles <m.t.eccles@hotmail.com>
2017-11-28 14:14:51 +1100

    Create sentry-userdata.sh

---
+++ sentry-userdata.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+yum update -y
+yum install -y docker
+usermod -a -G docker ec2-user
+curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+service docker start
+
+yum install -y git
+git clone https://github.com/getsentry/onpremise.git
+cd onpremise/
+mkdir -p data/{sentry,postgres}
+
+/usr/local/bin/docker-compose run --rm web config generate-secret-key > SECRET_KEY
+
+skey=$(<SECRET_KEY)
+sed -i "s/# SENTRY_SECRET_KEY: ''/SENTRY_SECRET_KEY: '"$skey"'/g" docker-compose.yml
+rm SECRET_KEY
+
+/usr/local/bin/docker-compose run --rm web upgrade --noinput
+
+/usr/local/bin/docker-compose run --rm web createuser --email sentry@fakeuser.com --password timmie1 --superuser
+
+/usr/local/bin/docker-compose up -d
